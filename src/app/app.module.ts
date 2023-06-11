a8fbafa6638314d8f51c692a92fb4fcdab05f51a
alexander98in <alexander98in@gmail.com>
2023-06-11 03:24:59 -0500

    Creacion de componentes y estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { ImgComponent } from './components/img/img.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ImgComponent,
+    ProductsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent {
+
+  //@Input() product!: Product;
+
+  @Input() product: Product = {
+    id: '',
+    name: '',
+    image: '',
+    price: 0
+  };
+
+  constructor() { }
+
+}
